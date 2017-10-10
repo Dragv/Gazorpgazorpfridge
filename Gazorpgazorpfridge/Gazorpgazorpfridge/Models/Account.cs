bdc310291eaff6590f914a8c9b1ac75cc6a8fc0a
Drag <ale_guereca_valdivia@hotmail.com>
2017-10-09 22:38:57 -0500

    Second commit
    Signed-off-by: Drag <ale_guereca_valdivia@hotmail.com>

---
+++ Gazorpgazorpfridge/Gazorpgazorpfridge/Models/Account.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Gazorpgazorpfridge.Models
+{
+    public class Account
+    {
+        public string correo { set; get; }
+        public string password { set; get; }
+        public Receta canastaBasica { set; get; }
+
+    }
+}
---
+++ Gazorpgazorpfridge/Gazorpgazorpfridge/Models/Refrigerador.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Gazorpgazorpfridge.Models
+{
+    public class Refrigerador
+    {
+        public string codigo { get; set; }
+        public IEnumerable<Producto> productosAlamcenados { get; set; }
+        public int capacidad { get; set; }
+        public int indiceDeEnfriamento { get; set; }
+    }
+}
