cfc047df8710cc05e98a3b36481f98f444238e48
Alejandro Güereca <ale_guereca_valdivia@hotmail.com>
2017-11-09 11:25:58 -0600

    Fixed data base and controllers issues

--- Gazorpgazorpfridge/Gazorpgazorpfridge/Models/IdentityModels.cs
+++ Gazorpgazorpfridge/Gazorpgazorpfridge/Models/IdentityModels.cs
@@ -40,18 +40,20 @@ namespace Gazorpgazorpfridge.Models
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             Database.SetInitializer(new MigrateDatabaseToLatestVersion<ApplicationDbContext, Configuration>());
+            modelBuilder.Entity<CanastaBasica>().ToTable("CanastasBasicas");
+            modelBuilder.Entity<Modelo>().ToTable("Modelos");
+            modelBuilder.Entity<Paquete>().ToTable("Paquetes");
+            modelBuilder.Entity<Producto>().ToTable("Productos");
+            modelBuilder.Entity<Receta>().ToTable("Recetas");
+            modelBuilder.Entity<Refrigerador>().ToTable("Refrigeradores");
             base.OnModelCreating(modelBuilder);
         }
 
-        public IDbSet<CanastaBasica> CanastasBasicas { get; set; }
-        public IDbSet<Modelo> Modelos { get; set; }
-        public IDbSet<Paquete> Paquetes { get; set; }
-        public IDbSet<Producto> Productos { get; set; }
-        public IDbSet<Receta> Recetas { get; set; }
-        public IDbSet<Refrigerador> Refrigeradores{ get; set; }
-
-        public System.Data.Entity.DbSet<Gazorpgazorpfridge.Models.Refrigerador> Refrigeradors { get; set; }
-
-        public System.Data.Entity.DbSet<Gazorpgazorpfridge.Models.Producto> Productoes { get; set; }
+        public DbSet<CanastaBasica> CanastasBasicas { get; set; }
+        public DbSet<Modelo> Modelos { get; set; }
+        public DbSet<Paquete> Paquetes { get; set; }
+        public DbSet<Producto> Productos { get; set; }
+        public DbSet<Receta> Recetas { get; set; }
+        public DbSet<Refrigerador> Refrigeradores{ get; set; }
     }
 }
